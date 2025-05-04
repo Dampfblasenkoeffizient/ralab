620db322e5385ca34b4c05f7ce8a41e9dfa73051
ClaraH23 <clara.heilig@uni-ulm.de>
2025-05-04 20:43:42 +0200

    bash

--- Scripts/RAM/sim_SP_RAM.sh
+++ Scripts/RAM/sim_SP_RAM.sh
@@ -1,2 +1,13 @@
 #!/bin/bash 
 # Clara Heilig
+
+ghdl --clean
+
+ghdl -a --std=08 Komponenten/RAM/Single_Port_RAM.vhdl
+ghdl -a --std=08 Komponenten/RAM/Single_Port_RAM_tb.vhdl
+
+ghdl -e --std=08 Testbenches/RAM/Single_Port_RAM_tb.vhdl
+
+ghdl -r --std=08 Komponenten/RAM/Single_Port_RAM_tb.vhdl
+
+gtkwave reg.vcd
