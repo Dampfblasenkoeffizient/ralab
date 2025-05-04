8c10f2bd2b6aa7c0b4f665f96d75043962a83287
ClaraH23 <clara.heilig@uni-ulm.de>
2025-05-04 20:37:15 +0200

    ram besser... gleich pruefen

--- Komponenten/RAM/Single_Port_RAM.vhdl
+++ Komponenten/RAM/Single_Port_RAM.vhdl
@@ -3,39 +3,40 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 library work;
-use work.types_package.all;
+use work.constant_package.all;
+
+
 
 entity Single_Port_RAM is
-    generic(
-        word_width : integer;
-        adr_width : integer 
-    );
     port(
         pi_clk : in std_logic;
         pi_rst : in std_logic;
-        pi_add : in std_logic_vector (adr_width -1 downto 0);
+        pi_add : in std_logic_vector (reg_adr_width -1 downto 0);
         pi_we : in std_logic;
-        po_data : out memory
+        pi_data : in std_logic_vector(word_width -1 downto 0);
+        po_data : out std_logic_vector(word_width -1 downto 0)
     );
 end entity;    
 
 architecture behavior of Single_Port_RAM is
+    type memory is array (0 to 3) of std_logic_vector (7 downto 0);
+    signal regs : memory := (others => (others => '0'));
     
     begin
         process(pi_clk, pi_rst, pi_we)
-        VARIABLE i : Integer := 0;
+        variable add : integer := integer(unsigned(pi_add));
 
-        if not rising_edge(pi_clk) -- do nothing
-        else
+            begin 
             if pi_rst = '1' then 
-                for i in 0 to 5 loop
-                    po_data (i) <= (others => '0');
-                end loop;    
-            else if pi_we = '1' then -- adrr wie?
-
-
-            end if;            
-        end if;            
+                    regs <= (others => (others => '0'));
+            else 
+                if rising_edge(pi_clk) then 
+                    if pi_we = '1' then 
+                        regs(add) <= pi_data;
+                    end if;            
+                end if;            
+            end if; 
+            po_data <= regs (add);
         end process;
     
 end behavior;
