804f7bd52de99c02052a4519defc4dc51a1318a6
ClaraH23 <clara.heilig@uni-ulm.de>
2025-05-05 14:22:25 +0200

    fix bash script

--- Scripts/RAM/sim_SP_RAM.sh
+++ Scripts/RAM/sim_SP_RAM.sh
@@ -7,8 +7,8 @@ ghdl -a --std=08 Packages/constant_package.vhdl
 ghdl -a --std=08 Komponenten/RAM/Single_Port_RAM.vhdl
 ghdl -a --std=08 Testbenches/RAM/Single_Port_RAM_tb.vhdl
 
-ghdl -e --std=08 Testbenches/RAM/Single_Port_RAM_tb.vhdl
+ghdl -e --std=08 Single_Port_RAM_tb
 
-ghdl -r --std=08 Testbenches/RAM/Single_Port_RAM_tb.vhdl
+ghdl -r --std=08 Single_Port_RAM_tb
 
 gtkwave reg.vcd
