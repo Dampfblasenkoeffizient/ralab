9a1206933dc31b59977a8106f313d9a811fb832b
ClaraH23 <clara.heilig@uni-ulm.de>
2025-05-09 14:11:37 +0200

    signExtension start

--- Komponenten/SignExtender/signExtension.vhdl
+++ Komponenten/SignExtender/signExtension.vhdl
@@ -1,9 +1,9 @@
 -- Laboratory RA solutions/versuch3
 -- Sommersemester 25
 -- Group Details
--- Lab Date:
--- 1. Participant First and Last Name: 
--- 2. Participant First and Last Name:
+-- Lab Date: 14.05.2025
+-- 1. Participant First and Last Name: Clara Heilig
+-- 2. Participant First and Last Name: Paul Riedel
 
 -- ========================================================================
 -- Description:  Sign extender for a RV32I processor. Takes the entire instruction
@@ -18,10 +18,32 @@ library ieee;
 
 entity signExtension is
 -- begin solution:
+    port(
+      pi_instr : in std_logic_vector(WORD_WIDTH - 1 downto 0);
+      po_immediateImm, po_storeImm, po_unsignedImm, po_branchImm, po_jumpImm : out std_logic_vector(WORD_WIDTH - 1 downto 0)
+    );
    -- end solution!!
 end entity signExtension;
 
 architecture arc of signExtension is
 -- begin solution:
+begin
+
+    po_immediateImm(11 downto 0) <= pi_instr(31 downto 20);
+    po_immediateImm(31 downto 12) <= (others => pi_instr(31));
+
+    po_storeImm(5 downto 0) <= pi_instr(11 downto 7);
+    po_storeImm(13 downto 6) <= pi_instr(31 downto 25);
+    po_storeImm(31 downto 14) <= (others => pi_instr(31));
+
+    po_unsignedImm(20 downto 0) <= pi_instr(31 downto 12);
+    po_unsignedImm(31 downto 21) <= (others => pi_instr(31));
+
+    po_branchImm(5 downto 0) <= pi_instr(11 downto 7);
+    po_branchImm(12 downto 6) <= pi_instr(31 downto 25);
+    po_branchImm(31 downto 13) <= (others => pi_instr(31));
+
+    po_jumpImm(20 downto 0) <= pi_instr(31 downto 12);
+    po_jumpImm(31 downto 21) <= (others => pi_instr(31));
  -- end solution!!
 end architecture arc;
