7e708eff44b97cc660ce3a51096386ad9301e67c
Paul <pauljridel@gmail.com>
2025-05-14 13:37:53 +0200

    the enum broke everything

--- Komponenten/Decoder/decoder.vhdl
+++ Komponenten/Decoder/decoder.vhdl
@@ -2,14 +2,15 @@
 -- Sommersemester 25
 -- Group Details
 -- Lab Date:
--- 1. Participant First and Last Name: 
--- 2. Participant First and Last Name:
+-- 1. Participant First and Last Name: Paul Riedel
+-- 2. Participant First and Last Name: Clara Heilig
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.Constant_package.all;
 use work.types.all;
+use ieee.std_logic_textio.all;
 
 entity decoder is
     generic (
@@ -24,43 +25,60 @@ end entity decoder;
 
 architecture arc of decoder is
     begin
-        process(pi_instruction, pi_clk)
+        process(pi_instruction)
             variable opcode : std_logic_vector(OPCODE_WIDTH - 1 downto 0);
             variable funct3 : std_logic_vector(2 downto 0);
             variable funct7 : std_logic_vector(6 downto 0);
+            variable v_insFormat : t_instruction_type;
         begin
-            if rising_edge(pi_clk) then
                 opcode := pi_instruction(6 downto 0);
                 funct3 := pi_instruction(14 downto 12);
                 funct7 := pi_instruction(31 downto 25);
 
+                case opcode is
+                    when R_INS_OP =>
+                        v_insFormat := bFormat;
+                    when LUI_INS_OP =>
+                        v_insFormat := uFormat;
+                    when AUIPC_INS_OP =>
+                        v_insFormat := uFormat;
+                    when JAL_INS_OP =>
+                        v_insFormat := jFormat;
+                    when B_INS_OP =>
+                        v_insFormat := bFormat;
+                    when S_INS_OP => 
+                        v_insFormat := sFormat;
+                    when JALR_INS_OP =>
+                        v_insFormat := iFormat;
+                    when L_INS_OP =>
+                        v_insFormat := iFormat;
+                    when I_INS_OP =>
+                        v_insFormat := iFormat;
+                    when others =>
+                        v_insFormat := nullFormat;
+                end case;
+
                 -- decoding the opcode component of the incomming instruction
 
-                case opcode is
-                    when R_INS_OP => 
+                case v_insFormat is
+                    when rFormat => 
                         -- register operations (r_type) set the alu_opcode based on the operation (func3) and the selector bit (func7(1))
                         -- the immediate select bit of the alu input mux is cleared since both inputs are registers
                         po_controlWord.ALU_OP <= funct7(5) & funct3;
                         po_controlWord.I_IMM_SEL <= '0';
-                    when I_INS_OP =>
+                    when iFormat =>
                         
-                    when L_INS_OP =>
+                    when uFormat =>
+                    
+                    when bFormat =>
                         
-                    when S_INS_OP =>
+                    when sFormat =>
                         
-                    when B_INS_OP =>
-                        
-                    when JAL_INS_OP =>
-                        
-                    when JALR_INS_OP =>
-                        
-                    when LUI_INS_OP =>
-                        
-                    when AUIPC_INS_OP =>
+                    when jFormat =>
 
-                    when others =>
+                    when nullFormat =>
                         po_controlWord <= control_word_init;
                 end case;
-            end if;
+
         end process;
 end architecture;
