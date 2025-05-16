41d596f7859a6efef13f02ce96499db1ad9e6626
ClaraH23 <clara.heilig@uni-ulm.de>
2025-05-16 09:02:13 +0200

    created po, renames s_storage to s_registers

--- Komponenten/Registerfile/Register_File.vhdl
+++ Komponenten/Registerfile/Register_File.vhdl
@@ -14,7 +14,8 @@ entity register_file is
         pi_clk, pi_rst, pi_writeEnable : in std_logic;
         pi_writeRegData : in std_logic_vector ((G_WORD_WIDTH -1) downto 0);
         pi_readRegAddr1, pi_readRegAddr2, pi_writeRegAddr : in std_logic_vector ((G_REG_ADR_WIDTH -1) downto 0);
-        po_readRegData1, po_readRegData2 : out std_logic_vector ((G_WORD_WIDTH -1) downto 0)
+        po_readRegData1, po_readRegData2 : out std_logic_vector ((G_WORD_WIDTH -1) downto 0);
+        po_registerOut : out registermemory
     );
 end register_file;
 
@@ -34,20 +35,21 @@ end register_file;
 
 architecture behavior of register_file is
 
-    signal s_storage : registermemory := (others => (others => '0'));
+    signal s_registers : registermemory := (others => (others => '0'));
     begin
         process(pi_clk, pi_rst)
         begin
             if pi_rst = '1' then
-                s_storage <= (others => (others => '0'));
+                s_registers <= (others => (others => '0'));
             else if rising_edge(pi_clk) then
                 if pi_writeEnable = '1' and (to_integer(unsigned(pi_writeRegAddr)) /= 0) then 
-                    s_storage(to_integer(unsigned(pi_writeRegAddr))) <= pi_writeRegData;
+                    s_registers(to_integer(unsigned(pi_writeRegAddr))) <= pi_writeRegData;
                 end if;
                 end if;
             end if;
-            po_readRegData1 <= s_storage(to_integer(unsigned(pi_readRegAddr1)));
-            po_readRegData2 <= s_storage(to_integer(unsigned(pi_readRegAddr2)));
+            po_readRegData1 <= s_registers(to_integer(unsigned(pi_readRegAddr1)));
+            po_readRegData2 <= s_registers(to_integer(unsigned(pi_readRegAddr2)));
+            po_registerOut <= s_registers;
         end process;
 
 end architecture;
