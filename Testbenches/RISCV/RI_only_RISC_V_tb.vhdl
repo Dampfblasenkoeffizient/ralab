bbcc12d1148e4a87a4dcc96502107fea5dece79f
Dampfblasenkoeffizient <108476606+Dampfblasenkoeffizient@users.noreply.github.com>
2025-05-28 14:52:09 +0200

    Update RI_only_RISC_V_tb.vhdl

--- Testbenches/RISCV/RI_only_RISC_V_tb.vhdl
+++ Testbenches/RISCV/RI_only_RISC_V_tb.vhdl
@@ -2,8 +2,8 @@
 -- Sommersemester 25
 -- Group Details
 -- Lab Date:
--- 1. Participant First and Last Name: 
--- 2. Participant First and Last Name:
+-- 1. Participant First and Last Name: Clara Heilig
+-- 2. Participant First and Last Name: Paul Riedel
 
 -- ========================================================================
 -- Author:       Marcel Rieß
